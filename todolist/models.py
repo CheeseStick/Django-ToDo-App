af0a48fb75837c1eb72d9e686bc46cc4968ba31e
Jun Hyeok Jung <junny@junjung.me>
2017-12-09 15:24:20 +0900

    ToDo List 모델 생성. todolist 어플리케이션 todo_app에 등록. (settings.py) .gitignore 파일에 프로젝트 관련 파일과 DB Migrations 관련 파일이 올라가지 않도록 추가.

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# 리스트
+class ToDoList(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+# 리스트 내 항목
+class ToDoListItem(models.Model):
+    list = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    created_date = models.DateTimeField(auto_created=True)
+    is_done = models.BooleanField(default=False)
